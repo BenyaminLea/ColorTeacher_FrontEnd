3d2b44b0c0f5702593dab98456b3c376716d86c4
batdrorLO <batdrorlo@gmail.com>
2021-01-26 15:47:15 +0200

    game-colorDetector

--- frontend/src/components/TakePicture/TakePicture.js
+++ frontend/src/components/TakePicture/TakePicture.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-
-
+import { ColorExtractor } from 'react-color-extractor'
+import { prominent } from 'color.js'
 class TakePicture extends Component {
     state = {
         imageURL: '',
+        colors: [],
+        mainColor: [],
+        save: false
     }
 
     videoEle = React.createRef();
     canvasEle = React.createRef();
     imageEle = React.createRef();
-
+    
     componentDidMount = async () => {
         this.startCamera();
     }
@@ -69,38 +72,113 @@ class TakePicture extends Component {
         })
     }
 
-    
+    saveImage = (e) => {
+        e.preventDefault();
+        const imageToCheck = this.state.imageURL;
+        prominent(imageToCheck, { amount: 1 }).then(color => {
+            console.log(color) // [241, 221, 63]
+            this.setState({mainColor: color})
+        })
+        this.setState({
+            save: true
+        })
+    };
+
+      renderSwatches = () => {
+    const { colors } = this.state
+
+    return colors.map((color, id) => {
+      return (
+        <div
+          key={id}
+          style={{
+            backgroundColor: color,
+            width: 100,
+            height: 100
+          }}
+        />
+      )
+    })
+  }
+
+
+    getColors = colors =>
+    this.setState(state => ({ colors: [...state.colors, ...colors] }))
 
     render() {
-        return (
-        <div >
-            {this.state.imageURL === '' && <div className="cam">
-                <video width="100%" height="100%" className="video-player" autoPlay={true} ref={this.videoEle}></video>
-                <button className="btn capture-btn" onClick={this.takeSelfie}>
-                    <i class="fa fa-camera" aria-hidden="true"></i>
-                </button>
-            </div>
-            }
-            <canvas ref={this.canvasEle} style={{display: 'none'}}></canvas>
-            {this.state.imageURL !== '' && <div className="preview">
-                <img className="preview-img" src={this.state.imageURL} ref={this.imageEle} />
+        if (this.state.save === true) {
+            return (
+                    <div>
+                        <ColorExtractor getColors={this.getColors}>
+                            <img
+                                src={this.state.imageURL}
+                                style={{ width: 700, height: 500 }}
+                            />
+                        </ColorExtractor>
+                    </div>)
+        }
+        else {
+            return (
+                <div >
+                    {this.state.imageURL === '' && <div className="cam">
+                        <video width="100%" height="100%" className="video-player" autoPlay={true} ref={this.videoEle}></video>
+                        <button className="btn capture-btn" onClick={this.takeSelfie}>
+                            <i className="fa fa-camera" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    }
+                    <canvas ref={this.canvasEle} style={{ display: 'none' }}></canvas>
+                    {this.state.imageURL !== '' && <div className="preview">
+                        <img className="preview-img" src={this.state.imageURL} ref={this.imageEle} />
 
-                <div className="btn-container">
-                    <button className="btn back-btn" onClick={this.backToCam}>
-                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                    </button>
-                    <a href={this.state.imageURL} download="selfie.png"
-                     className="btn download-btn">
-                        <i class="fa fa-download" aria-hidden="true"></i>
-                    </a>
-                </div>
-            </div>
-            }
-        </div>)
+                        <div className="btn-container">
+                            <button className="btn back-btn" onClick={this.backToCam}>
+                                <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                            </button>
+                            <button className="btn download-btn" onClick={this.saveImage}>
+                                <i className="fa fa-download" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
+                    }
+                </div>)
+        }
     }
 }
 
 export default TakePicture;
 
+
+/*
+
+<a href={this.state.imageURL} download="selfie.png" className="btn download-btn">
+    <i className="fa fa-download" aria-hidden="true"></i>
+</a>
+
+  imageUpload = (e) => {
+      const file = e.target.files[0];
+      getBase64(file).then(base64 => {
+        localStorage["fileBase64"] = base64;
+        console.debug("file stored",base64);
+      });
+  };
+
+  render() {
+    return <input 
+     type="file" 
+     id="imageFile" 
+     name='imageFile' 
+     onChange={this.imageUpload} />;
+  }
+}
+const getBase64 = (file) => {
+  return new Promise((resolve,reject) => {
+     const reader = new FileReader();
+     reader.onload = () => resolve(reader.result);
+     reader.onerror = error => reject(error);
+     reader.readAsDataURL(file);
+  });
+}
+*/
 
 
--- frontend/src/components/game/Game.jsx
+++ frontend/src/components/game/Game.jsx
@@ -36,8 +36,6 @@ function Game(props) {
                           <Speech text="Bring me something blue!"/>
                            <br></br>
                            <TakePicture/>
-                          <div style={{ backgroundColor: "red", height: "10rem", width: "10rem", borderRadius: "6px", margin: "1rem 0" }}>  
-                          </div>
                         </CardTitle>
                         <p />
                       </div>
@@ -47,98 +45,9 @@ function Game(props) {
               </Card>
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="8" xs="7">
-                      <div className="numbers" style={{ textAlign: "left" }}>
-                        <p className="card-category">A</p>
-                        <CardTitle tag="p">
-                          Blue
-                    </CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="8" xs="7">
-                      <div className="numbers" style={{ textAlign: "left" }}>
-                        <p className="card-category">B</p>
-                        <CardTitle tag="p">
-                          Green
-                    </CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="8" xs="7">
-                      <div className="numbers" style={{ textAlign: "left" }}>
-                        <p className="card-category">C</p>
-                        <CardTitle tag="p">
-                          Red
-                    </CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-
-              </Card>
-            </Col>
-            <Col>
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="8" xs="7">
-                      <div className="numbers" style={{ textAlign: "left" }}>
-                        <p className="card-category">D</p>
-                        <CardTitle tag="p">
-                          Black
-                    </CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          {/* <div className="update ml-auto mr-auto">
-            <Button
-              className="btn-round"
-              color="primary"
-              type="submit"
-            >
-              Update Profile
-            </Button>
-          </div> */}
         </Col>
       </Row>
     </div >
-
-
-    /* <div style={{ marginTop: "10rem" }}>
-      <h1>hello im a game</h1>
-      <div>{question}</div>
-      <button onClick={onClick}>dfs</button>
-    </div> */
   );
 }
 
